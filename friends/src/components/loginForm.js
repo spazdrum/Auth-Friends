95621437bf261c02a647622d0bc83b9aa362a8b5
spazdrum <spazdrum@gmail.com>
2020-08-07 22:10:43 -0400

    loginForm

---
+++ friends/src/components/loginForm.js
@@ -0,0 +1,59 @@
+import React from "react";
+import axiosWithAuth from "../utilities/axiosWithAuth";
+
+class loginForm extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: "",
+    },
+  };
+
+  handleChanges = (e) => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  login = (e) => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("api/login", this.state.credentials)
+      .then((res) => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/protected");
+      })
+      .catch((err) => {
+        console.log("There is an error", err);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChanges}
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChanges}
+            placeholder="Password"
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default loginForm;
